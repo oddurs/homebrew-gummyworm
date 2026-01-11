64e68e6f7d93ee0f3b5b815e5f010741e27fbdde
Oddur Sigurdsson <oddurs@gmail.com>
2026-01-11 02:33:11 -0500

    Update gummyworm to v2.1.1

--- Formula/gummyworm.rb
+++ Formula/gummyworm.rb
@@ -4,9 +4,9 @@
 class Gummyworm < Formula
   desc "gummyworm - Transform images into glorious ASCII art"
   homepage "https://github.com/oddurs/gummyworm"
-  url "https://github.com/oddurs/gummyworm/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "7571534179db2df2786ddf76f0efb820a6a2ae4fba33b5bea63e854d4085079c"
-  version "2.1.0"
+  url "https://github.com/oddurs/gummyworm/archive/refs/tags/v2.1.1.tar.gz"
+  sha256 "3393e1f4ee7a578baec23799bf9c69c147c3cac601dd01be58a1f42ec9e6b370"
+  version "2.1.1"
   license "MIT"
   head "https://github.com/oddurs/gummyworm.git", branch: "main"
 
